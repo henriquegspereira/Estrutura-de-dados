83424c10270db0a2c25b35f1dd6cc0edd599fb73
henriquegspereira <henriquegpereira07@gmail.com>
2025-10-15 19:40:14 -0300

    Aula 8

---
+++ Aula 8 - Bubble Sort/BubbleSort/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ordenação com Bubble Sort");
+
+int[] arrNumbers = new int[] { 99, 50, -24, 0, 1 };
+
+foreach (var number in arrNumbers)
+{
+    Console.Write($"[{number}] ");
+}
+
+var arrOrdered =
+    BubbleSort.BubbleSortOrder.Sort(arrNumbers);
+
+Console.WriteLine("");
+Console.WriteLine("\n*****************");
+Console.WriteLine("Vetor ordenado: ");
+Console.WriteLine("");
+foreach (var number in arrOrdered)
+{
+    Console.Write($"[{number}] ");
+}
---
+++ Aula 8 - Bubble Sort/VetorStringBubble/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Ordenação com Bubble Sort");
+
+string[] arrStrings = new string[] {
+    "Ana",
+    "Carlos",
+    "Zenaide",
+    "João",
+    "Beatriz",
+    "Maria",
+    "Pedro",
+    "Juliana",
+    "Gustavo"
+};
+
+foreach (var nome in arrStrings)
+{
+    Console.Write($"[{nome}] ");
+}
+
+var arrOrdered = VetorStringBubble.BubbleSortOrder.Sort<string>(arrStrings);
+
+Console.WriteLine("");
+Console.WriteLine("\n*****************");
+Console.WriteLine("Vetor ordenado: ");
+Console.WriteLine("");
+foreach (var nome in arrOrdered)
+{
+    Console.Write($"[{nome}] ");
+}
