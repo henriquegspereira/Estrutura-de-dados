0f6483cc10704e09e287ca917171cfd75a2853d6
henriquegspereira <henriquegpereira07@gmail.com>
2025-10-16 22:54:56 -0300

    Aula 9

---
+++ Aula 9 - Selection Sort/SelectionSort/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+SelectionSort.SelectionSort.Sort(integerValues);
+Console.WriteLine("Vetor de Inteiros");
+Console.WriteLine(string.Join(", ", integerValues));
+
+Console.WriteLine("********************************");
+
+Console.WriteLine("Vetor de Strings");
+string[] stringValues = { "Bolsonaro", "Cleber", "Putin",
+"Pica-Pau", "Matuê", "Barack Hussein Obana", "Tais Carla" };
+
+SelectionSort.SelectionSort.Sort(stringValues);
+Console.WriteLine(string.Join(", ", stringValues));
