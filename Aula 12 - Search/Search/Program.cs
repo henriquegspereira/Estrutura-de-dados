8fe6359c0b742e04a917b5d85ad5ca16429e15b6
henriquegspereira <henriquegpereira07@gmail.com>
2025-11-12 20:29:56 -0300

    Adicionando aula 12

---
+++ Aula 12 - Search/Search/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program {
+
+    public static void Main()
+    {
+        string[] storage = { "artur", "1", "carlos", "daniel", "e", "fernando", "7", "8", "indio", "jaca" };
+
+        bool encontrado = false;
+
+        Console.WriteLine("Qual elemento você deseja buscar: ");
+
+        string? numeroProcurado = Console.ReadLine();
+
+        for (int i = 0; i < storage.Length; i++)
+        {
+            if (storage[i] == numeroProcurado)
+            {
+                Console.WriteLine("Elemento encontrado na posição: " + i);
+                encontrado = true;
+                break;
+            }
+        }   
+
+        if (!encontrado)
+        {
+            Console.WriteLine("Elemento não encontrado");
+        }
+    }
+}
